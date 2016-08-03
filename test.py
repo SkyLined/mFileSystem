073bebbeeb037453b0ae2a6c249c0cd91ef172b2
SkyLined <berendjanwever@gmail.com>
2016-08-03 15:40:07 +0200

    Add test for ASCII mode

--- test.py
+++ test.py
@@ -7,54 +7,56 @@ sSpecialChars = 'test[[[\0\r\n"<>\\/?*:|]]]';
 
 sSpecialCharsPath = FileSystem.fsFullPath(sTempFolderPath, sSpecialChars);
 
-print "* Testing file operations with special characters..."; 
-sTranslatedSpecialChars = FileSystem.fsTranslateToValidName(sSpecialChars);
-sData = "x";
+for bUnicode in [True, False]:
+  print "* Testing file operations with special characters in %s mode..." % (bUnicode and "Unicode" or "ASCII"); 
+  sTranslatedSpecialChars = FileSystem.fsTranslateToValidName(sSpecialChars, bUnicode = bUnicode);
+  print u"  File/folder name: %s" % sTranslatedSpecialChars;
+  sData = "x";
 
-# Clean old files/folders
-print "* Cleaning up old files/folders..."; 
-print "  * Checking if old test file exists..."; 
-ebIsFileResult = FileSystem.febIsFile(sTempFolderPath, sTranslatedSpecialChars);
-assert isinstance(ebIsFileResult, bool), "Expected febIsFile result to be a boolean, got %s" % repr(ebIsFileResult);
-if ebIsFileResult:
-  print "  * Deleting old test file..."; 
+  # Clean old files/folders
+  print "  * Cleaning up old files/folders..."; 
+  print "    * Checking if old test file exists..."; 
+  ebIsFileResult = FileSystem.febIsFile(sTempFolderPath, sTranslatedSpecialChars);
+  assert isinstance(ebIsFileResult, bool), "Expected febIsFile result to be a boolean, got %s" % repr(ebIsFileResult);
+  if ebIsFileResult:
+    print "    * Deleting old test file..."; 
+    ebDeleteFileResult = FileSystem.febDeleteFile(sTempFolderPath, sTranslatedSpecialChars);
+    assert ebDeleteFileResult is True, "Expected febDeleteFile result to be True, got %s" % repr(ebDeleteFileResult);
+  print "    * Checking if old test folder exists..."; 
+  ebIsFolderResult = FileSystem.febIsFolder(sTempFolderPath, sTranslatedSpecialChars);
+  assert isinstance(ebIsFolderResult, bool), "Expected febIsFolder result to be a boolean, got %s" % repr(ebIsFolderResult);
+  if ebIsFolderResult:
+    print "    * Deleting old test folder..."; 
+    ebDeleteFolderResult = FileSystem.febDeleteFolder(sTempFolderPath, sTranslatedSpecialChars);
+    assert ebDeleteFolderResult is True, "Expected febDeleteFolder result to be True, got %s" % repr(ebDeleteFolderResult);
+
+  print "  * Creating file with special characters..."; 
+  # Create a file, detect it, read it and delete it.
+  eWriteFileResult = FileSystem.feWriteDataToFile(sData, sTempFolderPath, sTranslatedSpecialChars);
+  assert eWriteFileResult is None, "Expected feWriteDataToFile result to be None, got %s" % repr(eWriteFileResult);
+  print "    * Checking if file with special characters exists..."; 
+  ebIsFileResult = FileSystem.febIsFile(sTempFolderPath, sTranslatedSpecialChars);
+  assert ebIsFileResult is True, "Expected febIsFile result to be True, got %s" % repr(ebIsFileResult);
+  print "    * Reading file with special characters..."; 
+  esReadFileResult = FileSystem.fesReadDataFromFile(sTempFolderPath, sTranslatedSpecialChars);
+  assert esReadFileResult == sData, "Expected febReadDataFromFile result to be %s, got %s" % (repr(sData), repr(esReadFileResult));
+  print "    * Deletiong file with special characters..."; 
   ebDeleteFileResult = FileSystem.febDeleteFile(sTempFolderPath, sTranslatedSpecialChars);
   assert ebDeleteFileResult is True, "Expected febDeleteFile result to be True, got %s" % repr(ebDeleteFileResult);
-print "  * Checking if old test folder exists..."; 
-ebIsFolderResult = FileSystem.febIsFolder(sTempFolderPath, sTranslatedSpecialChars);
-assert isinstance(ebIsFolderResult, bool), "Expected febIsFolder result to be a boolean, got %s" % repr(ebIsFolderResult);
-if ebIsFolderResult:
-  print "  * Deleting old test folder..."; 
+
+  print "  * Testing folder operations with special characters...";
+  # Create a folder, detect it, create a file in the folder and delete the folder.
+  print "    * Creating folder with special characters..."; 
+  ebCreateFolderResult = FileSystem.febCreateFolder("x", sTempFolderPath, sTranslatedSpecialChars);
+  assert ebCreateFolderResult is True, "Expected febCreateFolder result to be True, got %s" % repr(ebCreateFolderResult);
+  print "    * Checking if folder with special characters exists..."; 
+  ebIsFolderResult = FileSystem.febIsFolder(sTempFolderPath, sTranslatedSpecialChars);
+  assert ebIsFolderResult is True, "Expected febIsFolder result to be True, got %s" % repr(ebIsFolderResult);
+  print "    * Creating file with special characters in folder with special characters..."; 
+  eWriteFileResult = FileSystem.feWriteDataToFile(sData, sTempFolderPath, sTranslatedSpecialChars, sTranslatedSpecialChars);
+  assert eWriteFileResult is None, "Expected feWriteDataToFile result to be None, got %s" % repr(eWriteFileResult);
+  print "    * Deletiong folder with special characters containing a file with special characters..."; 
   ebDeleteFolderResult = FileSystem.febDeleteFolder(sTempFolderPath, sTranslatedSpecialChars);
   assert ebDeleteFolderResult is True, "Expected febDeleteFolder result to be True, got %s" % repr(ebDeleteFolderResult);
-
-print "* Creating file with special characters..."; 
-# Create a file, detect it, read it and delete it.
-eWriteFileResult = FileSystem.feWriteDataToFile(sData, sTempFolderPath, sTranslatedSpecialChars);
-assert eWriteFileResult is None, "Expected feWriteDataToFile result to be None, got %s" % repr(eWriteFileResult);
-print "  * Checking if file with special characters exists..."; 
-ebIsFileResult = FileSystem.febIsFile(sTempFolderPath, sTranslatedSpecialChars);
-assert ebIsFileResult is True, "Expected febIsFile result to be True, got %s" % repr(ebIsFileResult);
-print "  * Reading file with special characters..."; 
-esReadFileResult = FileSystem.fesReadDataFromFile(sTempFolderPath, sTranslatedSpecialChars);
-assert esReadFileResult == sData, "Expected febReadDataFromFile result to be %s, got %s" % (repr(sData), repr(esReadFileResult));
-print "  * Deletiong file with special characters..."; 
-ebDeleteFileResult = FileSystem.febDeleteFile(sTempFolderPath, sTranslatedSpecialChars);
-assert ebDeleteFileResult is True, "Expected febDeleteFile result to be True, got %s" % repr(ebDeleteFileResult);
-
-print "* Testing folder operations with special characters...";
-# Create a folder, detect it, create a file in the folder and delete the folder.
-print "  * Creating folder with special characters..."; 
-ebCreateFolderResult = FileSystem.febCreateFolder("x", sTempFolderPath, sTranslatedSpecialChars);
-assert ebCreateFolderResult is True, "Expected febCreateFolder result to be True, got %s" % repr(ebCreateFolderResult);
-print "  * Checking if folder with special characters exists..."; 
-ebIsFolderResult = FileSystem.febIsFolder(sTempFolderPath, sTranslatedSpecialChars);
-assert ebIsFolderResult is True, "Expected febIsFolder result to be True, got %s" % repr(ebIsFolderResult);
-print "  * Creating file with special characters in folder with special characters..."; 
-eWriteFileResult = FileSystem.feWriteDataToFile(sData, sTempFolderPath, sTranslatedSpecialChars, sTranslatedSpecialChars);
-assert eWriteFileResult is None, "Expected feWriteDataToFile result to be None, got %s" % repr(eWriteFileResult);
-print "  * Deletiong folder with special characters containing a file with special characters..."; 
-ebDeleteFolderResult = FileSystem.febDeleteFolder(sTempFolderPath, sTranslatedSpecialChars);
-assert ebDeleteFolderResult is True, "Expected febDeleteFolder result to be True, got %s" % repr(ebDeleteFolderResult);
 
 print "+ All tests passed."
