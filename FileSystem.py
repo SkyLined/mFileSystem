f3ff7fca62fe26424ced7e701917bb0c946074f5
SkyLined <github@skylined.nl>
2017-05-12 12:01:32 +0200

    Update versioning system

--- FileSystem.py
+++ FileSystem.py
@@ -1,6 +1,7 @@
 import os, shutil, time;
 # To solve any unicode errors you may get, add this line (without the "#") to your main python script:
 # import codecs,sys; sys.stdout = codecs.getwriter("cp437")(sys.stdout, "replace");
+from oVersionInformation import oVersionInformation;
 
 # Try again almost immediately, then wait longer and longer
 auPauses = [
--- __init__.py
+++ __init__.py
@@ -1,2 +1,1 @@
 from FileSystem import *;
-from fsVersionCheck import fsVersionCheck;
